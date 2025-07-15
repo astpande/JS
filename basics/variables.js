48fa7bc758b2a11c4d206206b39d67b2c7900655
Astha <154652571+astpande@users.noreply.github.com>
2025-07-15 11:10:09 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 155236
+let accountEmail = "astha@gmail.com"
+var accountPasword = "142563"
+accountCity = "Lucknow"
+
+//accountId = 2 not allowed
+accountEmail = "fc@gmail.com"
+accountPasword = "4587458"
+accountCity = "jaipur"
+
+console.log(accountId);
+console.table ([accountId,accountPasword,accountEmail,accountCity])
