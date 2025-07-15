d9336cb13785911892981d6ed868a19d02f1b38e
Astha <154652571+astpande@users.noreply.github.com>
2025-07-15 13:04:03 +0000

    converstions

---
+++ basics/cover.js
@@ -0,0 +1,18 @@
+let score = '55xyz'
+console.log (typeof score)
+
+
+let valueInNumber = Number (score)
+console.log (typeof valueInNumber)
+console.log (valueInNumber)
+
+
+let isloggedIN = 1
+let booleanisloggedIN = Boolean (isloggedIN)
+console.log(booleanisloggedIN)
+
+let somenum = 99
+
+let stringNumber = String (somenum)
+console.log(stringNumber)
+console.log(typeof stringNumber)
